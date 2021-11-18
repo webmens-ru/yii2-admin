bb8467c0225fc376543ce1d9c611e089b7f3a835
Lukoyanov-aa <Lukoyanov_aa@webmens.ru>
2021-11-18 19:55:43 +0300

    add RestController

---
+++ src/controllers/RestController.php
@@ -0,0 +1,48 @@
+<?php
+namespace wm\admin\controllers;
+
+use Yii;
+use yii\filters\auth\HttpBearerAuth;
+
+class RestController extends \yii\rest\Controller
+{
+
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+
+        return [
+            'corsFilter' => [
+                'class' => \yii\filters\Cors::class,
+                'cors' => [
+                    // restrict access to
+                    'Origin' => ['http://localhost:3000'],
+                    // Allow only POST and PUT methods
+                    'Access-Control-Request-Method' => ['POST', 'PUT', 'PATCH', 'DELETE', 'GET', 'OPTIONS'],
+                    // Allow only headers 'X-Wsse'
+                    'Access-Control-Request-Headers' => ['X-Wsse'],
+                    // Allow credentials (cookies, authorization headers, etc.) to be exposed to the browser
+                    'Access-Control-Allow-Credentials' => true,
+                    // Allow OPTIONS caching
+                    'Access-Control-Max-Age' => 3600,
+                    // Allow the X-Pagination-Current-Page header to be exposed to the browser.
+                    'Access-Control-Expose-Headers' => ['X-Pagination-Current-Page'],
+                    'Access-Control-Allow-Origin' => ['*'],
+                    'Access-Control-Allow-Headers' => ['*'],
+                ],
+            ],
+            'contentNegotiator' => [
+                'class' => \yii\filters\ContentNegotiator::class,
+                'formatParam' => '_format',
+                'formats' => [
+                    'application/json' => \yii\web\Response::FORMAT_JSON,
+                    'xml' => \yii\web\Response::FORMAT_XML,
+                ],
+            ],
+            'authenticator' => [
+                'class' => HttpBearerAuth::class,
+            ],
+        ];
+    }
+
+}
