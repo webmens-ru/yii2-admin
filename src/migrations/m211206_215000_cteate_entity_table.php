19a54b906944f72454d9d2978f5f9ede9e4e31b2
Lukoyanov-aa <Lukoyanov_aa@webmens.ru>
2021-12-06 22:20:37 +0300

    entity migrate

---
+++ src/migrations/m211206_215000_cteate_entity_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use yii\db\Migration;
+
+class m211206_215000_cteate_entity_table extends Migration
+{
+    public function up()
+    {
+        //admin_agents
+        $this->createTable('{{%admin_entity%}}', [
+            'code' => $this->string(32)->notNull(),
+            'name' => $this->string(255)->notNull(),
+
+        ]);
+        $this->addPrimaryKey('code', 'admin_entity', 'code');
+    }
+
+    public function down()
+    {
+        $this->dropTable('{{%admin_entity%}}');
+    }
+}
