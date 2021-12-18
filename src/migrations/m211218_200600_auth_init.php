821ab8eb86cecf6f167f011d46bce2bd2adfd75d
Lukoyanov-aa <Lukoyanov_aa@webmens.ru>
2021-12-18 20:24:18 +0300

    auth migrate

---
+++ src/migrations/m211218_200600_auth_init.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\db\Migration;
+
+class m211218_200600_auth_init extends Migration {
+
+    public function up()
+    {
+
+        $this->batchInsert('auth_item',
+            [
+                'name',
+                'type',
+                'rule_name',
+                'data',
+                'created_at',
+                'updated_at',
+            ],
+            [
+                ['admin', 1, 'Администратор', NULL, NULL, NULL, NULL],
+                ['canAdmin', 2, 'Право входа в админку', NULL, NULL, NULL, NULL],
+            ]
+        );
+
+        $this->batchInsert('auth_item',
+            [
+                'parent',
+                'child',
+            ],
+            [
+                ['admin', 'canAdmin'],
+            ]
+        );
+    }
+}
