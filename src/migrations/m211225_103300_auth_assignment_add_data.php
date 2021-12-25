9227c27d8c9b97c2726ec5b31fb789bd10808bb2
Lukoyanov-aa <Lukoyanov_aa@webmens.ru>
2021-12-25 10:44:57 +0300

    migrate auth_assignment data

---
+++ src/migrations/m211225_103300_auth_assignment_add_data.php
@@ -0,0 +1,21 @@
+<?php
+
+use yii\db\Migration;
+
+class m211225_103300_auth_assignment_add_data extends Migration {
+
+    public function up()
+    {
+
+        $this->batchInsert('auth_assignment',
+            [
+                'item_name',
+                'user_id',
+                'created_at',
+            ],
+            [
+                ['canAdmin', 1, NULL],
+            ]
+        );
+    }
+}
