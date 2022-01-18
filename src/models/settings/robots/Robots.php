2816fd5c94c17af6144cabb530a76c738c5676be
Lukoyanov-aa <Lukoyanov_aa@webmens.ru>
2022-01-18 11:16:53 +0300

    +

--- src/models/settings/robots/Robots.php
+++ src/models/settings/robots/Robots.php
@@ -127,7 +127,8 @@ class Robots extends \yii\db\ActiveRecord {
             }
             $result['properties'][] = $propertyArr;
         }
-        $file = '../controllers/' . Inflector::id2camel($this->handler) . 'Action.php';
+        //$file = '../controllers/' . Inflector::id2camel($this->handler) . 'Action.php';
+        $file = '../controllers/handlers/robot/generateQrCodeAction.php';
         Yii::warning($file, '$file');
         $zip = new \ZipArchive();
         $tempName = mt_rand(100000, 999999) . '.zip';
