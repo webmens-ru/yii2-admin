effac0f013369e90cabad12b50c688366ccba0c6
Lukoyanov-aa <Lukoyanov_aa@webmens.ru>
2022-01-19 16:26:56 +0300

    add migration add_data_filter_field_type

---
+++ src/migrations/m220119_162100_add_data_filter_field_type.php
@@ -0,0 +1,30 @@
+<?php
+
+use yii\db\Migration;
+
+class m220119_162100_add_data_filter_field_type extends Migration {
+
+    public function up() {
+        $this->batchInsert('admin_filter_field_type',
+            [
+                'id',
+                'name'
+            ],
+            [
+                [1, 'string'],
+                [2, 'number'],
+                [3, 'date'],
+                [4, 'select'],
+                [5, 'integer'],
+                [6, 'multiple_select_dynamic'],
+                [7, 'multiple_select'],
+                [8, 'select_dynamic'],
+            ]
+
+        );
+    }
+
+    public function down() {
+    }
+
+}
