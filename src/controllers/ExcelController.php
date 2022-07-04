64803120b40d16ab52e6968794a8bee39b122d58
Luk <33396443+lukoyanov-aa@users.noreply.github.com>
2022-07-04 17:27:13 +0300

    fix(Excel): Исправлена ошибка определения имени столбца

--- src/controllers/ExcelController.php
+++ src/controllers/ExcelController.php
@@ -132,7 +132,7 @@ class ExcelController extends Controller
         $letter = chr(65 + $numeric);
         $num2 = intval(($num - 1) / 26);
         if ($num2 > 0) {
-            return getNameFromNumber($num2) . $letter;
+            return self::getNameFromNumber($num2) . $letter;
         } else {
             return $letter;
         }
