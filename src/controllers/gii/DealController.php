c63d4b6d328fb87ccffc873ad7a41142db983a13
Luk <33396443+lukoyanov-aa@users.noreply.github.com>
2022-07-14 16:03:19 +0300

    add controllers

---
+++ src/controllers/gii/DealController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace wm\admin\controllers\gii;
+
+use wm\admin\models\gii\deal\TableGenerator;
+use Yii;
+use yii\filters\AccessControl;
+use yii\filters\VerbFilter;
+use yii\helpers\ArrayHelper;
+
+/**
+ * RobotsController implements the CRUD actions for Robots model.
+ */
+class DealController extends \wm\admin\controllers\BaseModuleController
+{
+
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['POST'],
+                ],
+            ],
+            'access' => [
+                'class' => AccessControl::className(),
+                //'only' => ['about'],
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => ['canAdmin'],
+                    ],
+                ],
+            ],
+        ];
+    }
+
+
+    public function actionTableGenerator()
+    {
+        $model = new TableGenerator();
+        $error = null;
+
+        if ($model->load(Yii::$app->request->post()) && $model->validate()) {
+            if($model->createTable()){
+                return $this->render('table-generator-confirm', [
+                    'model' => $model,
+                ]);
+            }else{
+                $error = 'Произошла ошибка при создании таблицы! Возможно таблица с таким именем уже существует';
+            }
+
+        }
+
+        $fieldsList = $model->getFieldsList();
+
+        return $this->render('table-generator', [
+            'fieldsList' => $fieldsList,
+            'error' => $error,
+            'model' => $model,
+        ]);
+    }
+}
