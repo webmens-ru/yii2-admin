15c9b778026b89e4a732b3f9fed7633df7c52ee7
RashnFlow <62694562+RashnFlow@users.noreply.github.com>
2022-08-23 14:18:58 +0300

    fix

--- src/migrations/m220816_150200_add_column_grid_column.php
+++ src/migrations/m220816_150200_add_column_grid_column.php
@@ -4,12 +4,11 @@ class m220816_150200_add_column_grid_column extends \yii\db\Migration
 {
     public function up()
     {
-        $this->update('{{%admin_grid_column}}', [
-            'frozen' => $this->boolean()->defaultValue(0),
-            'reordering' => $this->boolean()->defaultValue(1),
-            'resizeble' => $this->boolean()->defaultValue(1),
-            'sortable' => $this->boolean()->defaultValue(1),
-        ]);
+        $this->addColumn('admin_grid_column', 'frozen', $this->boolean()->defaultValue(0));
+        $this->addColumn('admin_grid_column', 'reordering', $this->boolean()->defaultValue(1));
+        $this->addColumn('admin_grid_column', 'resizeble', $this->boolean()->defaultValue(1));
+        $this->addColumn('admin_grid_column', 'sortable', $this->boolean()->defaultValue(1));
+
     }
 
     public function down()
