f363536cc00b253a97a0c68d7020d5407ba41ce2
RashnFlow <62694562+RashnFlow@users.noreply.github.com>
2022-08-29 10:40:57 +0300

    fix: Исправление ммиграции params

--- src/migrations/m220822_140500_update_admin_grid_action.php
+++ src/migrations/m220822_140500_update_admin_grid_action.php
@@ -4,9 +4,7 @@ class m220822_140500_update_admin_grid_action extends \yii\db\Migration
 {
     public function up()
     {
-        $this->update('{{%admin_grid_action}}', [
-            'params' => $this->text(),
-        ]);
+        $this->alterColumn('admin_grid_action', 'params', 'text');
     }
 
     public function down()
