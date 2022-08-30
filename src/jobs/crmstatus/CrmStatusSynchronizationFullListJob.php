5fc9121e299f486a9ca4b728f9b3cd29464de416
Luk <33396443+lukoyanov-aa@users.noreply.github.com>
2022-08-30 18:50:54 +0300

    feat: Добавлена поддержка синхронизации справочников

---
+++ src/jobs/crmstatus/CrmStatusSynchronizationFullListJob.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace wm\admin\jobs\crmstatus;
+
+use Bitrix24\B24Object;
+use wm\b24tools\b24Tools;
+use Yii;
+use yii\base\BaseObject;
+use yii\helpers\ArrayHelper;
+
+
+class CrmStatusSynchronizationFullListJob extends BaseObject implements \yii\queue\JobInterface
+{
+    public $modelClass;
+
+    public function execute($queue)
+    {
+        $this->modelClass::deleteAll();
+        $modelCrmStatus = Yii::createObject($this->modelClass);
+        $fieldsCrmStatus = $modelCrmStatus->attributes();
+        $component = new b24Tools();
+        $b24App = $component->connectFromAdmin();
+        $b24Obj = new B24Object($b24App);
+        $listDataSelector = 'result';
+        $params = [];
+        $request = $b24Obj->client->call(
+            'crm.status.list',
+            $params
+        );
+        foreach (ArrayHelper::getValue($request, $listDataSelector) as $oneEntity) {
+            $model = Yii::createObject($this->modelClass);
+            $model->loadData($oneEntity);
+        }        
+    }
+}
---
+++ src/models/synchronization/CrmStatus.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace wm\admin\models\synchronization;
+
+use Bitrix24\B24Object;
+use wm\admin\models\settings\Agents;
+use wm\admin\models\settings\events\Events;
+use wm\admin\jobs\crmstatus\CrmStatusSynchronizationFullListJob;
+use wm\admin\jobs\crmstatus\CrmStatusSynchronizationFullGetJob;
+use wm\admin\jobs\crmstatus\CrmStatusSynchronizationDeltaJob;
+use wm\b24tools\b24Tools;
+use Yii;
+use yii\db\Schema;
+use yii\helpers\ArrayHelper;
+
+class CrmStatus extends BaseEntity implements SynchronizationInterface
+{
+    public static function tableName()
+    {
+        return 'sync_crm_status';
+    }
+
+    public static $synchronizationFullListJob = CrmStatusSynchronizationFullListJob::class;
+
+    public static $synchronizationDeltaJob = CrmStatusSynchronizationDeltaJob::class;
+
+    public static $synchronizationFullGetJob = CrmStatusSynchronizationFullGetJob::class;
+
+    public static $primaryKeyColumnName = 'ID';
+
+
+    public static function getCountB24()
+    {
+        $component = new b24Tools();
+        $b24App = $component->connectFromAdmin();
+        $b24Obj = new B24Object($b24App);
+        $request = $b24Obj->client->call(
+            'crm.status.list',
+//            ['select' => ['ID']]
+        );
+        return $request['total'];
+    }
+
+    public static function getB24Fields()
+    {
+        $cache = Yii::$app->cache;
+        $key = 'crm.status.fields';
+        $fields = $cache->getOrSet($key, function () {
+            $component = new b24Tools();
+            $b24App = $component->connectFromAdmin();
+            $b24Obj = new B24Object($b24App);
+            $data = ArrayHelper::getValue($b24Obj->client->call(
+                'crm.status.fields'
+            ), 'result');
+            return $data;
+        }, 300);
+        return $fields;
+    }
+
+    public static function getB24FieldsList()
+    {
+        $result = [];
+        foreach (self::getB24Fields() as $key => $value) {
+            $result[$key] = ArrayHelper::getValue($value, 'formLabel') ?: ArrayHelper::getValue($value, 'title');
+        }
+        return $result;
+    }
+
+    public static function startSynchronization($modelAgentTimeSettings)
+    {
+        $agent = Agents::find()->where(['class' => static::class, 'method' => 'synchronization'])->one();
+        if (!$agent) {
+            $agent = new Agents();
+            $agent->name = 'Синхронизация дельты справочников';
+            $agent->class = static::class;
+            $agent->method = 'synchronization';
+            $agent->params = '-';
+            $agent->date_run = '1970-01-01 00:00:00';
+        }
+        $agent->load(ArrayHelper::toArray($modelAgentTimeSettings), '');
+        $agent->status_id = 1;
+        $agent->save();
+    }
+
+    public static function stopSynchronization()
+    {
+        $agent = Agents::find()->where(['class' => static::class, 'method' => 'synchronization'])->one();
+        if ($agent) {
+            $agent->status_id = 0;
+            $agent->save();
+        }
+    }
+
+    public function loadData($data)
+    {
+        foreach ($data as $key => $val) {
+            if (in_array($key, array_keys($this->attributes))) {
+                is_array($val) ? $this->$key = json_encode($val) : $this->$key = $val;
+            }
+        }
+        $this->save();
+        if ($this->errors) {
+            Yii::error($this->errors, 'CrmStatus->loadData()');
+        }
+    }
+}
