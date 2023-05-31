e9b9187fd839a5092f6d5443f57ec1c64cf6c2ba
Lukoyanov-aa <Lukoyanov_aa@webmens.ru>
2023-05-31 18:32:19 +0300

    fix: TaskElapsedItemSynchronizationFullJob

--- src/jobs/taskElapsedItem/TaskElapsedItemSynchronizationFullListJob.php
+++ src/jobs/taskElapsedItem/TaskElapsedItemSynchronizationFullListJob.php
@@ -4,11 +4,13 @@ namespace wm\admin\jobs\taskElapsedItem;
 
 use Bitrix24\B24Object;
 use Bitrix24\Bitrix24Entity;
+use wm\admin\models\synchronization\TasklElapsedItem;
 use wm\b24tools\b24Tools;
 use yii\base\BaseObject;
 use yii\helpers\ArrayHelper;
 use yii\queue\JobInterface;
 use wm\admin\jobs\taskElapsedItem\TaskElapsedItemSynchronizationFullJob as FullJob;
+use Yii;
 
 class TaskElapsedItemSynchronizationFullListJob extends BaseObject implements JobInterface
 {
@@ -17,17 +19,18 @@ class TaskElapsedItemSynchronizationFullListJob extends BaseObject implements Jo
 
     public function execute($queue) //TODO до 50-ти сотрудников
     {
-
         $this->modelClass::deleteAll(/*['>=', 'CREATED_DATE' => date('Y-m-d', strtotime('-30 days'))]*/);
         $modelTask = Yii::createObject($this->modelClass);
         $fieldsTask = $modelTask->attributes();
         $component = new b24Tools();
-        \Yii::$app->params['logPath'] = 'log/';
+        Yii::$app->params['logPath'] = 'log/';
         $b24App = $component->connectFromAdmin();
         $b24Obj = new B24Object($b24App);
         $listDataSelector = 'result';
         $params = [
-//            'select' => $fieldsTask,
+            "ORDER" => array("ID" => "ASC"),
+            "FILTER" => ['>ID' => 1],
+            "SELECT" => ['*'],
         ];
         $request = $b24Obj->client->call(
             'task.elapseditem.getlist',
@@ -40,18 +43,14 @@ class TaskElapsedItemSynchronizationFullListJob extends BaseObject implements Jo
         $countCalls = (int)ceil($request['total'] / $b24Obj->client::MAX_BATCH_CALLS);
         $data = ArrayHelper::getValue($request, $listDataSelector);
         if (count($data) != $request['total']) {
-            for ($i = 1; $i < $countCalls; $i++) {
+            for ($i = 2; $i <= $countCalls; $i++) {
+                $params['PARAMS']['NAV_PARAMS'] = [
+                    'nPageSize' => 50,
+                    'iNumPage' => $i
+                ];
                 $b24Obj->client->addBatchCall(
                     'task.elapseditem.getlist',
-                    array_merge(
-                        $params,
-                        [
-                            'PARAMS' => [
-                                'nPageSize' => 50,
-                                'iNumPage' => $i
-                            ]
-                        ]
-                    ),
+                    $params,
                     function ($result) use ($listDataSelector) {
                         foreach (ArrayHelper::getValue($result, $listDataSelector) as $oneEntity) {
                             $model = Yii::createObject($this->modelClass);
@@ -62,24 +61,5 @@ class TaskElapsedItemSynchronizationFullListJob extends BaseObject implements Jo
             }
             $b24Obj->client->processBatchCalls();
         }
-//        foreach ($this->userIds as $userId) {
-//            $listDataSelector = 'result';
-//            $params = [
-//                'type' => 'user',
-//                'ownerId' => $userId,
-//                'from' => $this->from,
-//            ];
-//            $request = $b24Obj->client->call(
-//                'calendar.event.get',
-//                $params
-//            );
-//            foreach (ArrayHelper::getValue($request, $listDataSelector) as $oneEntity) {
-//                $model = $this->modelClass::find()->where(['ID' => $oneEntity['ID']])->one();
-//                if (!$model) {
-//                    $model = Yii::createObject($this->modelClass);
-//                }
-//                $model->loadData($oneEntity);
-//            }
-//        }
     }
 }
