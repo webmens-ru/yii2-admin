0369da3520fec7471721fe50f85f1e76e9ef9d8a
Lukoyanov-aa <Lukoyanov_aa@webmens.ru>
2023-08-19 20:36:40 +0300

    fix:() Исправлена ошибка генерации  CRUD

--- src/models/gii/model/react/model.php
+++ src/models/gii/model/react/model.php
@@ -117,7 +117,7 @@ class <?= $className ?> extends <?= '\\' . ltrim($generator->baseClass, '\\') . 
                     'type' => 'openApplication',
                     "params" => [
                         'path' => 'mainForm',
-                        'bx24_width' => 700,
+                        'width' => 700,
                         'updateOnCloseSlider' => true,
                         "params" => [
                             'entity' => '<?= $generator->crudController ?>',
@@ -133,7 +133,7 @@ class <?= $className ?> extends <?= '\\' . ltrim($generator->baseClass, '\\') . 
                 'type' => 'openApplication',
                 "params" => [
                     'path' => 'mainForm',
-                    'bx24_width' => 700,
+                    'width' => 700,
                     'updateOnCloseSlider' => true,
                     "params" => [
                         'entity' => '<?= $generator->crudController ?>',
