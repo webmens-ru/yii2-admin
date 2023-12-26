c0e494bf6c11de8558a9948df7a114f9e61aae21
Lukoyanov-aa <Lukoyanov_aa@webmens.ru>
2023-12-26 10:49:44 +0300

    fix:() исправлен способ получения на start = -1

--- src/jobs/company/CompanySynchronizationFullListJob.php
+++ src/jobs/company/CompanySynchronizationFullListJob.php
@@ -21,35 +21,51 @@ class CompanySynchronizationFullListJob extends BaseObject implements \yii\queue
         $component = new b24Tools();
         \Yii::$app->params['logPath'] = 'log/';
         $b24App = $component->connectFromAdmin();
-        $b24Obj = new B24Object($b24App);
+        $obB24 = new B24Object($b24App);
         $listDataSelector = 'result';
         $params = [
+            'order' => ["ID" => "ASC"],
             'select' => $fieldsDeal,
+            //'filter' => ['>ID'=> 133553],
         ];
-        $request = $b24Obj->client->call(
-            'crm.company.list',
-            $params
-        );
-        foreach (ArrayHelper::getValue($request, $listDataSelector) as $oneEntity) {
-            $model = Yii::createObject($this->modelClass);
-            $model->loadData($oneEntity);
-        }
-        $countCalls = (int)ceil($request['total'] / $b24Obj->client::MAX_BATCH_CALLS);
-        $data = ArrayHelper::getValue($request, $listDataSelector);
-        if (count($data) != $request['total']) {
-            for ($i = 1; $i < $countCalls; $i++) {
-                $b24Obj->client->addBatchCall(
-                    'crm.company.list',
-                    array_merge($params, ['start' => $b24Obj->client::MAX_BATCH_CALLS * $i]),
-                    function ($result) use ($listDataSelector) {
-                        foreach (ArrayHelper::getValue($result, $listDataSelector) as $oneEntity) {
-                            $model = Yii::createObject($this->modelClass);
-                            $model->loadData($oneEntity);
-                        }
-                    }
-                );
+        $request = $obB24->client->call('crm.company.list', $params);
+        $countCalls = (int)ceil($request['total'] / $obB24->client::MAX_BATCH_CALLS);
+        $countBatchCalls = (int)ceil($countCalls / 50);
+        $maxId = 0;
+        for ($j = 0; $j < $countBatchCalls; $j++) {
+            if ($j == 0) {
+                $prevId = 0;
+            } else {
+                $prevId = $maxId;
             }
-            $b24Obj->client->processBatchCalls();
+            for ($i = 0; $i < 50; $i++) {
+                if ($countCalls > 0) {
+                    $idx = $obB24->client->addBatchCall(
+                        'crm.company.list',
+                        [
+                            'order' => ["ID" => "ASC"],
+                            'filter' => [
+                                '>ID' => $prevId
+                            ],
+                            'select' => $fieldsDeal,
+                            'start' => -1
+                        ],
+                        function ($result) use ($listDataSelector, &$maxId) {
+                            Yii::$app->db->close();
+                            Yii::$app->db->open();
+                            $data = ArrayHelper::getValue($result, $listDataSelector);
+                            foreach ($data as $oneEntity) {
+                                $model = Yii::createObject($this->modelClass);
+                                $model->loadData($oneEntity);
+                            }
+                            $maxId = max(ArrayHelper::getColumn($data, 'ID'));
+                        }
+                    );
+                    $prevId = '$result[' . $idx . '][49][ID]';
+                }
+                $countCalls--;
+            }
+            $obB24->client->processBatchCalls();
         }
     }
 }
--- src/jobs/deal/DealSynchronizationFullListJob.php
+++ src/jobs/deal/DealSynchronizationFullListJob.php
@@ -21,37 +21,51 @@ class DealSynchronizationFullListJob extends BaseObject implements \yii\queue\Jo
         $component = new b24Tools();
         \Yii::$app->params['logPath'] = 'log/';
         $b24App = $component->connectFromAdmin();
-        $b24Obj = new B24Object($b24App);
+        $obB24 = new B24Object($b24App);
         $listDataSelector = 'result';
         $params = [
+            'order' => ["ID" => "ASC"],
             'select' => $fieldsDeal,
+            //'filter' => ['>ID'=> 133553],
         ];
-        $request = $b24Obj->client->call(
-            'crm.deal.list',
-            $params
-        );
-        foreach (ArrayHelper::getValue($request, $listDataSelector) as $oneEntity) {
-            $model = Yii::createObject($this->modelClass);
-            $model->loadData($oneEntity);
-        }
-        $countCalls = (int)ceil($request['total'] / $b24Obj->client::MAX_BATCH_CALLS);
-        $data = ArrayHelper::getValue($request, $listDataSelector);
-        if (count($data) != $request['total']) {
-            for ($i = 1; $i < $countCalls; $i++) {
-                $b24Obj->client->addBatchCall(
-                    'crm.deal.list',
-                    array_merge($params, ['start' => $b24Obj->client::MAX_BATCH_CALLS * $i]),
-                    function ($result) use ($listDataSelector) {
-                        Yii::$app->db->close();
-                        Yii::$app->db->open();
-                        foreach (ArrayHelper::getValue($result, $listDataSelector) as $oneEntity) {
-                            $model = Yii::createObject($this->modelClass);
-                            $model->loadData($oneEntity);
-                        }
-                    }
-                );
+        $request = $obB24->client->call('crm.deal.list', $params);
+        $countCalls = (int)ceil($request['total'] / $obB24->client::MAX_BATCH_CALLS);
+        $countBatchCalls = (int)ceil($countCalls / 50);
+        $maxId = 0;
+        for ($j = 0; $j < $countBatchCalls; $j++) {
+            if ($j == 0) {
+                $prevId = 0;
+            } else {
+                $prevId = $maxId;
             }
-            $b24Obj->client->processBatchCalls();
+            for ($i = 0; $i < 50; $i++) {
+                if ($countCalls > 0) {
+                    $idx = $obB24->client->addBatchCall(
+                        'crm.deal.list',
+                        [
+                            'order' => ["ID" => "ASC"],
+                            'filter' => [
+                                '>ID' => $prevId
+                            ],
+                            'select' => $fieldsDeal,
+                            'start' => -1
+                        ],
+                        function ($result) use ($listDataSelector, &$maxId) {
+                            Yii::$app->db->close();
+                            Yii::$app->db->open();
+                            $data = ArrayHelper::getValue($result, $listDataSelector);
+                            foreach ($data as $oneEntity) {
+                                $model = Yii::createObject($this->modelClass);
+                                $model->loadData($oneEntity);
+                            }
+                            $maxId = max(ArrayHelper::getColumn($data, 'ID'));
+                        }
+                    );
+                    $prevId = '$result[' . $idx . '][49][ID]';
+                }
+                $countCalls--;
+            }
+            $obB24->client->processBatchCalls();
         }
     }
 }
